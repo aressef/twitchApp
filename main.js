47f36d890864f3ef9aa7a45e22657590c2b2b576
Ryan Fuhrman <rsfuhrman10@gmail.com>
2017-07-10 21:46:26 -0400

    Add AJAX request structure

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
- 
+var xhr = new XMLHttpRequest();
+xhr.open('GET', '');
+xhr.send(null);
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    if (xhr.status === 200) {
+      console.log(xhr.responseText);
+    } else {
+      console.log('Error: ' + xhr.status);
+    }
+  }
+};

aee878f9b607c1f785c37b3fa95b3d1607ff04d1
Ryan Fuhrman <rsfuhrman10@gmail.com>
2017-07-26 16:43:03 -0400

    Add ajax request to twitch api

--- main.js
+++ main.js
@@ -1,14 +1,51 @@
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', '');
-xhr.send(null);
+// AJAX Request to Twitch.tv API
 
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4) {
-    if (xhr.status === 200) {
-      console.log(xhr.responseText);
-    } else {
-      console.log('Error: ' + xhr.status);
-    }
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
   }
-};
+
+  return xhr;
+}
+
+function makeCORSRequest(){
+  var url = 'https://wind-bow.glitch.me/twitch-api/users/freecodecamp';
+
+  var xhr = createCORSRequest('GET', url);
+  if (!xhr) {
+    throw new Error('CORS not supported');
+  }
+
+  xhr.onload = function() {
+  var responseText = xhr.responseText;
+  console.log(responseText);
+  // process the response.
+  };
+
+  xhr.onerror = function() {
+   console.log('There was an error!');
+  };
+
+  xhr.send();
+
+}
+
+makeCORSRequest();

2cff43912a95f13b5eccab5fc1ce4c5b52d6667e
Ryan Fuhrman <rsfuhrman10@gmail.com>
2017-07-28 15:36:40 -0400

    Restructure main.js to use methods

--- main.js
+++ main.js
@@ -3,57 +3,55 @@ var userArray = ["freecodecamp", "ESL_SC2", "BikeMan", "Dazss", "storbeck", "hab
 
 
 // AJAX Request to Twitch.tv API
+var communicationWithTwitchAPI = {
+  createCORSRequest: function(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
 
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
+      // Check if the XMLHttpRequest object has a "withCredentials" property.
+      // "withCredentials" only exists on XMLHTTPRequest2 objects.
+      xhr.open(method, url, true);
 
-    // Check if the XMLHttpRequest object has a "withCredentials" property.
-    // "withCredentials" only exists on XMLHTTPRequest2 objects.
-    xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
 
-  } else if (typeof XDomainRequest != "undefined") {
+      // Otherwise, check if XDomainRequest.
+      // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+      xhr = new XDomainRequest();
+      xhr.open(method, url);
 
-    // Otherwise, check if XDomainRequest.
-    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
+    } else {
 
-  } else {
+      // Otherwise, CORS is not supported by the browser.
+      xhr = null;
 
-    // Otherwise, CORS is not supported by the browser.
-    xhr = null;
-
-  }
-
-  return xhr;
-}
-
-function makeCORSRequest(){
-
-  userArray.forEach(function(user) {
-    var url = 'https://wind-bow.glitch.me/twitch-api/users/' + user;
-
-    var xhr = createCORSRequest('GET', url);
-    if (!xhr) {
-      throw new Error('CORS not supported');
     }
 
-    xhr.onload = function() {
-      var responseText = xhr.responseText;
-      var parsed = JSON.parse(responseText);
-      console.log(parsed);
-    };
+    return xhr;
+  },
+  makeCORSRequest: function() {
+    userArray.forEach(function(user) {
+      var url = 'https://wind-bow.glitch.me/twitch-api/users/' + user;
 
-    xhr.onerror = function() {
-     console.log('There was an error!');
-    };
+      var xhr = communicationWithTwitchAPI.createCORSRequest('GET', url);
+      if (!xhr) {
+        throw new Error('CORS not supported');
+      }
 
-    xhr.send();
+      xhr.onload = function() {
+        var responseText = xhr.responseText;
+        var parsed = JSON.parse(responseText);
+        console.log(parsed);
+      };
 
-  });
+      xhr.onerror = function() {
+       console.log('There was an error!');
+      };
+
+      xhr.send();
+
+    });
+  }
+};
 
 
-}
-
-makeCORSRequest();
+communicationWithTwitchAPI.makeCORSRequest();

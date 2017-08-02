90ec2fbf98d25113446ffadf46739e982441b769
Ryan Fuhrman <rsfuhrman10@gmail.com>
2017-08-01 20:39:19 -0400

    Add usernames to DOM after AJAX call

--- main.js
+++ main.js
@@ -39,9 +39,13 @@ var communicationWithTwitchAPI = {
       }
 
       xhr.onload = function() {
-        var responseText = xhr.responseText;
-        parsed = JSON.parse(responseText);
-        queryResults.push(parsed);
+        responseText = JSON.parse(xhr.responseText);
+        queryResults.push(responseText);
+
+        if (queryResults.length == userArray.length) {
+          display.user();
+          // display.bio();
+        }
       };
 
       xhr.onerror = function() {
@@ -51,31 +55,43 @@ var communicationWithTwitchAPI = {
       xhr.send();
 
     });
-
     console.log(queryResults);
   }
 };
 
-var display = {
-  users: function() {
-    var userList = document.querySelector('.userList');
 
+var display = {
+
+  user: function() {
+    var userList = document.querySelector('.userList');
     for (var i = 0; i < userArray.length; i++) {
       var userName = document.createElement('li');
       userName.className = 'userName';
       userName.textContent = queryResults[i].display_name;
       userList.appendChild(userName);
     }
-
+  // },
+  // bio: function() {
+  //   var userList = document.querySelector('.userName');
+  //   for (var i = 0; i < userArray.length; i++) {
+  //     var bio = document.createElement('li');
+  //     bio.className = 'bio';
+  //     bio.textContent = queryResults[i].bio;
+  //     user.appendChild(bio);
+  //   }
   }
 };
 
-
-
-let runAfterAjax = new Promise((resolve, reject) => {
-  setTimeout(communicationWithTwitchAPI.makeCORSRequest() {
-    resolve("Finished!");
-  }, 100);
-});
-
-runAfterAjax.then(display.users());
+communicationWithTwitchAPI.makeCORSRequest();
+// function runAfterAjax() {
+//   var ajaxPromise = new Promise(function(resolve, reject) {
+//     communicationWithTwitchAPI.makeCORSRequest();
+//     resolve('finished');
+//   });
+//
+//   Promise.all(ajaxPromise).then(display.users());
+//
+// };
+//
+//
+// runAfterAjax();
